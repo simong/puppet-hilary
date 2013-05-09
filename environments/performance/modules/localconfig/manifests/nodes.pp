f4f6febfbe4a9175f05a65d434faacab2305f3b8
Simon Gaeremynck <gaeremyncks@gmail.com>
2013-05-09 17:00:51 +0100

    Performance update.

--- environments/performance/modules/localconfig/manifests/nodes.pp
+++ environments/performance/modules/localconfig/manifests/nodes.pp
@@ -118,23 +118,23 @@ node 'search1' inherits search {
 ## REDIS NODES ##
 #################
 
-node 'cache-master' inherits cache {
-  $nodesuffix = '-master'
+node 'cache0' inherits cache {
+  $nodesuffix = 0
   hiera_include(classes)
 }
 
-node 'cache-slave' inherits cache {
-  $nodesuffix = '-slave'
+node 'cache1' inherits cache {
+  $nodesuffix = 1
   hiera_include(classes)
 }
 
-node 'activity-cache-master' inherits activity-cache {
-  $nodesuffix = '-master'
+node 'activity-cache0' inherits activity-cache {
+  $nodesuffix = 0
   hiera_include(classes)
 }
 
-node 'activity-cache-slave' inherits activity-cache {
-  $nodesuffix = '-slave'
+node 'activity-cache1' inherits activity-cache {
+  $nodesuffix = 1
   hiera_include(classes)
 }
 
@@ -142,13 +142,13 @@ node 'activity-cache-slave' inherits activity-cache {
 ## MESSAGING NODES ##
 #####################
 
-node 'mq-master' inherits mq {
-  $nodesuffix = '-master'
+node 'mq0' inherits mq {
+  $nodesuffix = 0
   hiera_include(classes)
 }
 
-node 'mq-slave' inherits mq {
-  $nodesuffix = '-slave'
+node 'mq1' inherits mq {
+  $nodesuffix = 1
   hiera_include(classes)
 }
 
@@ -177,12 +177,12 @@ node 'pp2' inherits pp {
 ## ETHERPAD NODES ##
 ####################
 
-node 'ep0' inherits ep {
+node 'etherpad0' inherits ep {
   $nodesuffix = 0
   hiera_include(classes)
 }
 
-node 'ep1' inherits ep {
+node 'etherpad1' inherits ep {
   $nodesuffix = 1
   hiera_include(classes)
 }
@@ -209,5 +209,11 @@ node 'bastion' {
   hiera_include(classes)
 }
 
+#################
+## DRIVER NODE ##
+#################
 
-
+node 'driver' {
+  $nodetype = 'driver'
+  hiera_include(classes)
+}
--- modules/oaeservice/manifests/driver.pp
+++ modules/oaeservice/manifests/driver.pp
@@ -1,5 +1,14 @@
 class oaeservice::driver {
-  class { 'tsung': }
+
+  require oaeservice::deps::common
+  require oaeservice::deps::package::git
+  require oaeservice::deps::package::nodejs
+
+  Class['::oaeservice::deps::common']                   -> Class['::tsung']
+  Class['::oaeservice::deps::package::git']             -> Class['::tsung']
+  Class['::oaeservice::deps::package::nodejs']          -> Class['::tsung']
+
+  class { '::tsung': }
 
   package { 'nginx':
     ensure    => present,
@@ -10,5 +19,35 @@ class oaeservice::driver {
     ensure  => running,
     enable  => true,
     require => Package['nginx'],
+  }
+
+  vcsrepo { '/opt/slapchop':
+    ensure    => latest,
+    provider  => git,
+    source    => "http://github.com/simong/slapchop",
+    revision  => "performance",
+  }
+
+  vcsrepo { '/opt/oae-nightly-stats':
+    ensure    => latest,
+    provider  => git,
+    source    => "http://github.com/simong/oae-nightly-stats",
+    revision  => "nightly-performance",
+  }
+
+  vcsrepo { '/opt/OAE-model-loader':
+    ensure    => latest,
+    provider  => git,
+    source    => "http://github.com/sakaiproject/OAE-model-loader",
+    revision  => "Hilary",
+  }
+
+  cron { 'nightly-performance-run':
+    ensure  => present,
+    command => "/opt/oae-nightly-stats/nightly-run.sh",
+    user    => 'root',
+    target  => 'root',
+    hour    => 4,
+    minute  => 0,
   }
 }
