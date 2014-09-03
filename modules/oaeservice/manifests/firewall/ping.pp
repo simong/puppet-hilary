078fe8f46219be77ea6a508d04d481aae4a8ad1b
Simon Gaeremynck <gaeremyncks@gmail.com>
2014-09-03 09:39:19 +0100

    Allow ICMP ping on QA1

---
+++ modules/oaeservice/manifests/firewall/ping.pp
@@ -0,0 +1,11 @@
+class oaeservice::firewall::ping {
+  # Accept ICMP pings on the public interface
+  iptables { '004 allow icmp pings':
+    chain   => 'INPUT',
+    iniface => 'eth0',
+    proto   => 'icmp',
+    state   => ['NEW', 'ESTABLISHED', 'RELATED'],
+    icmp    => 8
+    jump    => 'ACCEPT',
+  }
+}
